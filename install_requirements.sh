9aa27d569e690cb716d34cbe8cf476bb68569c58
Dmitry Ponomarev <ponomarevda96@gmail.com>
2022-04-18 12:27:54 +0300

    move installation from readme into install_requirements.sh script and add missed packages

---
+++ install_requirements.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt-get install -y  libopencv-dev
+apt-get install -y  libopenmpi-dev
+
+# PCL installation.
+# ENV variable DEBIAN_FRONTEND=noninteractive should be set,
+# otherwise it could require to interactivly choose the settings for keyboard-configuration package 
+DEBIAN_FRONTEND=noninteractive apt-get install -y libpcl-dev
+
+# Eigen installation. Since the package include Eigen headers as <Eigen/...> instead of <eigen3/Eigen/Core>, let's create a symbolic.
+sudo apt-get install -y libeigen3-dev
+sudo ln -s /usr/include/eigen3/Eigen /usr/include/Eigen
