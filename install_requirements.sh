88a5ae4cce9540f1d9ede34814b23b8cf1e850c8
Dmitry Ponomarev <ponomarevda96@gmail.com>
2022-04-18 13:13:10 +0300

    ros: add template

--- install_requirements.sh
+++ install_requirements.sh
@@ -11,3 +11,12 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y libpcl-dev
 # Eigen installation. Since the package include Eigen headers as <Eigen/...> instead of <eigen3/Eigen/Core>, let's create a symbolic.
 sudo apt-get install -y libeigen3-dev
 sudo ln -s /usr/include/eigen3/Eigen /usr/include/Eigen
+
+# Install ROS requirements
+apt-get install -y  python3-pip                             \
+                    python3-lxml                            \
+                    psmisc                                  \
+                    ros-$ROS_DISTRO-rviz                    \
+                    ros-$ROS_DISTRO-tf                      \
+                    ros-$ROS_DISTRO-tf2-ros                 \
+                    ros-$ROS_DISTRO-rosbash
---
+++ src/node.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file node.cpp
+ * @author ponomarevda96@gmail.com
+ */
+
+#include <ros/ros.h>
+
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "node");
+    ros::NodeHandle nh;
+
+    return 0;
+}
