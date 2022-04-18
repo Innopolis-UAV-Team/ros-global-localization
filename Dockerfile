bfd5092cea1d934b5a2d6f2f3b03f74f8e9a591e
Dmitry Ponomarev <ponomarevda96@gmail.com>
2022-04-18 13:13:24 +0300

    docker: add dockerfile and script (docker wrapper) how use build/run with docker

---
+++ Dockerfile
@@ -0,0 +1,32 @@
+ARG ROS_DISTRO=noetic
+
+FROM ros:$ROS_DISTRO
+LABEL description="HPC Registration approach for global localization ROS package"
+LABEL maintainer="ponomarevda96@gmail.com"
+SHELL ["/bin/bash", "-c"]
+WORKDIR /catkin_ws/src/hybrid_pc_registration
+RUN apt-get update                          &&  \
+    apt-get upgrade -y                      &&  \
+    apt-get install -y  git                     \
+                        ros-$ROS_DISTRO-catkin  \
+                        python3-catkin-tools
+
+RUN if [[ "$ROS_DISTRO" = "melodic" ]] ; then apt-get install -y python-pip python-catkin-tools ; fi
+
+# Install hybrid_pc_registration package
+COPY install_requirements.sh    install_requirements.sh
+RUN ./install_requirements.sh
+
+# COPY repository
+COPY include/ include/
+COPY libs/ libs/
+COPY src/ src/
+COPY test/ test/
+COPY CMakeLists.txt CMakeLists.txt
+COPY package.xml package.xml
+
+# Build
+RUN source /opt/ros/$ROS_DISTRO/setup.bash  && cd ../../ && catkin build
+
+CMD echo "main process has been started"            &&  \
+    echo "container has been finished"
---
+++ docker.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+print_help() {
+   echo "Wrapper under docker API for HPCR approach for global localization ROS package.
+It encapsulates all necessary docker flags for working with the package and properly handles image versions.
+https://tfs.university.innopolis.ru/tfs/IndustrialRoboticsLab/Colocalization/_git/ros-global-localization
+
+usage: docker.sh [build | pull | push | run | test | interactive | kill | help]
+
+Commands:
+build           Build docker image.
+pull            Pull docker image.
+push            Push docker image.
+run             Run inclinometer
+rviz            Run inclinometer with rviz.
+test            Run tests
+interactive     Run container in interactive mode.
+kill            Kill all containers.
+help            Print this message and exit"
+}
+
+setup_config() {
+    TAG_NAME=v0.0.1
+    DOCKERHUB_REPOSITOTY=industrial_robotics_lab/hpcr
+    if uname -m | grep -q 'aarch64'; then
+        TAG_NAME="$TAG_NAME""arm64"
+    elif uname -m | grep -q 'x86_64'; then
+        TAG_NAME="$TAG_NAME""amd64"
+    else
+        echo "unknown architecture"
+        exit
+    fi
+    DOCKER_CONTAINER_NAME=$DOCKERHUB_REPOSITOTY:$TAG_NAME
+
+    DOCKER_FLAGS="--privileged -v $DEV_PATH_SYMLINK:$DEV_PATH_SYMLINK       \
+                 --net=host                                                 \
+                 -v "/tmp/.X11-unix:/tmp/.X11-unix:rw"                      \
+                 -e DISPLAY=$DISPLAY                                        \
+                 -e QT_X11_NO_MITSHM=1)"
+
+    echo "TAG_NAME is" $TAG_NAME
+    echo "DOCKERHUB_REPOSITOTY is" $DOCKERHUB_REPOSITOTY
+}
+
+build_docker_image() {
+    setup_config
+    docker build -t $DOCKER_CONTAINER_NAME .
+}
+
+pull_docker_image() {
+    setup_config
+    docker pull $DOCKER_CONTAINER_NAME
+}
+
+push_docker_image() {
+    setup_config
+    docker push $DOCKER_CONTAINER_NAME
+}
+
+run() {
+    setup_config
+    echo "Not ready yet"
+}
+
+run_rviz() {
+    setup_config
+    echo "Not ready yet"
+}
+
+test() {
+    setup_config
+    echo "Not ready yet"
+}
+
+run_interactive() {
+    setup_config
+    echo "Not ready yet"
+}
+
+kill_all_containers() {
+    docker kill $(sudo docker ps -q)
+}
+
+
+cd "$(dirname "$0")"
+
+if [ "$1" = "build" ]; then
+    build_docker_image
+elif [ "$1" = "pull" ]; then
+    pull_docker_image
+elif [ "$1" = "push" ]; then
+    push_docker_image
+elif [ "$1" = "run" ]; then
+    run
+elif [ "$1" = "rviz" ]; then
+    run_rviz
+elif [ "$1" = "test" ]; then
+    test
+elif [ "$1" = "interactive" ]; then
+    run_interactive
+elif [ "$1" = "kill" ]; then
+    kill_all_containers
+else
+    print_help
+fi
